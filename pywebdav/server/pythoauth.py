7f7afdd43ca610f03ce040981fe88b6316cb9a52
Denis Brandolini <denis.brandolini@prometeia.com>
2020-02-22 12:08:04 +0100

    Integrazione minimale con pythoauth #1
    NB: il path accessibile è cablato in base alla convenzione

--- pywebdav/server/pythoauth.py
+++ pywebdav/server/pythoauth.py
@@ -13,6 +13,10 @@ def get_ttl_hash(seconds=TTL_HASH_SECS):
     return int(time.time()) // seconds
 
 
+class FailedPythoAuth(Exception):
+    pass
+
+
 class PythoAuthHandler(DAVAuthHandler):
     timeout = 3000
 
@@ -23,30 +27,47 @@ class PythoAuthHandler(DAVAuthHandler):
         url = self._config.DAV.pythoauthserver + '/api/v0/' + resource
         args = dict(args)
         args.setdefault('timeout', self.timeout)
+        self._log(f"Pytho request {method} {resource}")
         try:
             ret = requests.request(method=method, url=url, **args)
         except (requests.ConnectionError, requests.exceptions.ReadTimeout):
-            log.exception("Failed pytho request %s %s", method, resource)
-            return
+            log.exception("PythoAuth connection error")
+            raise FailedPythoAuth(f"Failed PythoAuth connection {method} {resource}")
         if ret.status_code // 100 not in [2, 4] or not ret.text.strip():
-            log.error("Failed auth with request %s %s cause unexpected response, code %s body %s".format(
-                method, url, ret.status_code, ret.text.strip()))
-            return
+            raise FailedPythoAuth(f"Failed PythoAuth with request {method} {resource} cause "
+                                  f"unexpected response, code {ret.status_code} body {ret.text.strip()}")
         if not ret.status_code // 100 == 2:
-            log.info("Failed auth against %s %s with status code %s", method, url, ret.status_code)
+            log.warning("Unauthorized request %s %s with status code %s", method, url, ret.status_code)
             return
         return ret.text.strip()
 
     def _pytho_ticket(self, username, password):
         return self._pytho_req('POST', 'auth', auth=(username, password), ttl_hash=get_ttl_hash())
 
+    def is_user_authorized(self, user, path, command):
+        # TODO: check command and path against API user details
+        if not user or not path:
+            return False
+        return path.split('/')[:3] == ['', user, 'media']
+
     def get_userinfo(self, user, pw, command):
         """ authenticate user """
         self._log(f"{command} from {user}")
-        res_useraname = self._pytho_req('GET', 'auth', auth=(user, pw), ttl_hash=get_ttl_hash())
-        if user and res_useraname and user == res_useraname:
-            self._log(f'Successfully authenticated user {user} for {command}')
-            return 1
-        self._log(f'Authentication failed for user {user}')
-        return
+        if not user or not pw:
+            self._log(f'Invalid Authentication parameters')
+            return
+        try:
+            res_useraname = self._pytho_req('GET', 'auth', auth=(user, pw), ttl_hash=get_ttl_hash())
+        except FailedPythoAuth as fe:
+            log.error(fe)
+            res_useraname = None
+        if not res_useraname or res_useraname != user:
+            self._log(f'Authentication failed for user {user}')
+            return
+        self._log(f'Successfully authenticated user {user} for {command}')
+        if not self.is_user_authorized(user, self.path, command):
+            self._log(f'User {user} not authorized for {command} on {self.path}')
+            return
+        self._log(f'Succesfully authorized {user} for {command} on {self.path}')
+        return 1
 
