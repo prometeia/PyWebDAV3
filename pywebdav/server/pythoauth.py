f4575f21e539f7ba213d5baebe45156d7198b2cc
Denis Brandolini <denis.brandolini@prometeia.com>
2020-02-25 18:23:27 +0100

    #5 integrate le API per ottenere la user folder
    Inoltre, gli admin ora possono accedere alla user area altrui.

--- pywebdav/server/pythoauth.py
+++ pywebdav/server/pythoauth.py
@@ -16,11 +16,13 @@ def get_ttl_hash(seconds=TTL_HASH_SECS):
 
 
 @lru_cache()
-def _pytho_rest_req(method, url, ttl_hash, **args):
+def _pytho_rest_req(method, url, ttl_hash, retjson=False, ticket=None, **args):
     assert ttl_hash
     log.debug(f"Pytho request {method} {url}")
     args = dict(args)
     args.setdefault('timeout', PYTHO_REQ_TIMEOUT)
+    if ticket:
+        args.setdefault('headers', {'doob-tkt': ticket})
     try:
         ret = requests.request(method=method, url=url, **args)
     except (requests.ConnectionError, requests.exceptions.ReadTimeout):
@@ -32,6 +34,8 @@ def _pytho_rest_req(method, url, ttl_hash, **args):
     if not ret.status_code // 100 == 2:
         log.warning("Unauthorized request %s %s with status code %s", method, url, ret.status_code)
         return
+    if retjson:
+        return ret.json()
     return ret.text.strip()
 
 
@@ -42,22 +46,36 @@ class FailedPythoAuth(Exception):
 class PythoAuthHandler(DAVAuthHandler):
     timeout = 3000
 
+    def _pytho_api_uri(self, resource='auth'):
+        return self._config.DAV.pythoauthserver + '/api/v0/' + resource
+
     def _pytho_auth_check(self, username, password):
-        url = self._config.DAV.pythoauthserver + '/api/v0/auth'
-        return _pytho_rest_req('GET', url, get_ttl_hash(), auth=(username, password))
+        tkt = _pytho_rest_req('POST', self._pytho_api_uri('auth'), get_ttl_hash(),
+                              auth=(username, password))
+        if not tkt:
+            return
+        assert isinstance(tkt, str)
+        userobj = _pytho_rest_req('GET', self._pytho_api_uri(f'users/{username}'),
+                                  get_ttl_hash(), retjson=True, ticket=tkt)
+        return userobj
 
     def _pytho_ticket_gen(self, username, password):
         url = self._config.DAV.pythoauthserver + '/api/v0/auth'
         return self._pytho_req('POST', url, get_ttl_hash(), auth=(username, password))
 
-    def is_user_authorized(self, user, path, command):
+    def is_user_authorized(self, userobj: dict, path, command):
         # TODO: check command and path against API user details
+        if not userobj or not isinstance(userobj, dict) or not userobj.get('user_dir'):
+            return False
+        isadmin = userobj['is_superuser']
 
         def is_my_area(tgpath):
-            if not user or not tgpath:
+            if not tgpath:
                 return False
-            target = self.IFACE_CLASS.basepath.split('/') + [user, 'media']
-            return tgpath.split('/')[:len(target)] == target
+            tgpp = tgpath.rstrip('/').split('/')
+            target = self.IFACE_CLASS.basepath.split('/') + userobj['user_dir'].rstrip('/').split('/')
+            return tgpp[:len(target)] == target \
+                   or isadmin and len(tgpp) == len(target) and tgpp[-1] == target[-1]
 
         if command in ('MKCOL'):
             return False
@@ -74,15 +92,15 @@ class PythoAuthHandler(DAVAuthHandler):
             self._log(f'Invalid Authentication parameters')
             return
         try:
-            res_useraname = self._pytho_auth_check(user, pw)
+            userobj = self._pytho_auth_check(user, pw)
         except FailedPythoAuth as fe:
             log.error(fe)
-            res_useraname = None
-        if not res_useraname or res_useraname != user:
+            userobj = None
+        if not userobj or userobj.get('username') != user:
             self._log(f'Authentication failed for user {user}')
             return 401
         self._log(f'Successfully authenticated user {user} for {command}')
-        if not self.is_user_authorized(user, self.path, command):
+        if not self.is_user_authorized(userobj, self.path, command):
             self._log(f'User {user} not authorized for {command} on {self.path}')
             return 403
         self._log(f'Succesfully authorized {user} for {command} on {self.path}')
